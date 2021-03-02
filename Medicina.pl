40e0762ebff23be3af04a97d5d1daa8ff61190b8
Patitax <71519272+Patitax@users.noreply.github.com>
2021-03-02 07:37:13 -0600

    Add files via upload

---
+++ Medicina.pl
@@ -0,0 +1,13 @@
+puede_ser(Medicina, Quimica).
+puede_ser(Medicina, Natural).
+
+causa(Quimica, Efectos_secundarios).
+ayuda(Quimica, Enfermedades_generales).
+son(Quimica, Costosas).
+compuesta_por(Quimica, Quimicos_y_estractos).
+forma(Quimica, Pastillas_o_infuciones).
+
+causa(Natural, Pocos_efectos_secundarios).
+ayuda(Natural, Enfermedades_comunes).
+son(Natural, Baratas).
+compuesta_por(Natural, Estractos_naturales).
